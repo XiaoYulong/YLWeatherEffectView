7d3f8eb242a73e80041bf014064495e490b4703b
XiaoYulong <xiaoyulong007@gmail.com>
2015-03-12 15:15:04 +0800

    add view

---
+++ YLWeatherEffectView/YLWeatherEffectView.h
@@ -0,0 +1,13 @@
+//
+//  YLWeatherEffectView.h
+//  YLWeatherEffectViewDemo
+//
+//  Created by 肖 玉龙 on 15/3/12.
+//  Copyright (c) 2015年 Yulong Xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YLWeatherEffectView : UIView
+
+@end
---
+++ YLWeatherEffectView/YLWeatherEffectView.m
@@ -0,0 +1,21 @@
+//
+//  YLWeatherEffectView.m
+//  YLWeatherEffectViewDemo
+//
+//  Created by 肖 玉龙 on 15/3/12.
+//  Copyright (c) 2015年 Yulong Xiao. All rights reserved.
+//
+
+#import "YLWeatherEffectView.h"
+
+@implementation YLWeatherEffectView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
